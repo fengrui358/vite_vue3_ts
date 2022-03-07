c0d71abda6df225d3db1c27f71e85458ead54dfa
fengrui <fengrui358@163.com>
2022-03-07 17:24:06 +0800

    chore: add http request

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/login',
+    path: '/',
     name: 'Login',
     component: () => import('@/pages/login/LoginView.vue'), // 注意这里要带上 文件后缀.vue
   },
