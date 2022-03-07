b19cf3a4fa2cb1db6a7d3ba4aa12b78f3335b70b
fengrui <fengrui358@163.com>
2022-03-07 17:34:58 +0800

    chore: add pinia

--- src/pages/login/LoginView.vue
+++ src/pages/login/LoginView.vue
@@ -1,3 +1,10 @@
 <template>
   <h1>登陆</h1>
+  <p>{{ mainStore.name }}</p>
 </template>
+
+<script setup lang="ts">
+import { useMainStore } from '@/store/main'
+
+const mainStore = useMainStore()
+</script>
