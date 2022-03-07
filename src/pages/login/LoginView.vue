e9d2e63b77e2cdf6fe472c1301206d03fcb0d03d
fengrui <fengrui358@163.com>
2022-03-07 17:58:54 +0800

    chore: direct modify name

--- src/pages/login/LoginView.vue
+++ src/pages/login/LoginView.vue
@@ -4,6 +4,8 @@
     用户名：{{ mainStore.name }}
     <br />
     长度: {{ mainStore.nameLength }}
+    <br />
+    <button @click="updateName">直接修改store中的name</button>
   </p>
 </template>
 
@@ -11,4 +13,11 @@
 import { useMainStore } from '@/store/main'
 
 const mainStore = useMainStore()
+
+const updateName = () => {
+  // $patch 修改 store 中的数据
+  mainStore.$patch({
+    name: '名称被修改了,nameLength也随之改变了',
+  })
+}
 </script>
