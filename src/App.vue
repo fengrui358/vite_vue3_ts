4d8e7c895edae9748a3c30c5e44309381ccde2ad
fengrui <fengrui358@163.com>
2022-03-08 14:46:23 +0800

    chore: config global theme and language

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import { NConfigProvider } from 'naive-ui'
+import { zhCN, dateZhCN, darkTheme as theme } from 'naive-ui'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <router-view></router-view>
+  <n-config-provider :locale="zhCN" :theme="theme">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <router-view></router-view>
+  </n-config-provider>
 </template>
 
 <style>
